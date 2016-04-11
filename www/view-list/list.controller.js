89565f012f595e8d8cae6297e358212fc2ed1c72
Ernesto Laval <ernesto.laval@king.com>
2016-04-11 07:14:03 +0100

    Group devices list

--- www/view-list/list.controller.js
+++ www/view-list/list.controller.js
@@ -6,12 +6,27 @@ angular.module('ble101')
 
   // keep a reference since devices will be added
   myself.devices = BLE.devices;
+  myself.devices101 = [];
+  myself.devicesOther = [];
+  
+
 
   var success = function () {
       if (myself.devices.length < 1) {
           // a better solution would be to update a status message rather than an alert
           alert("Didn't find any Bluetooth Low Energy devices.");
       }
+      
+    myself.devices101 = [];
+    myself.devicesOther = [];
+      
+    _.each(BLE.devices, function(d) {
+      if (d.advertising.kCBAdvDataLocalName == "BLE101") {
+        myself.devices101.push(d);
+      } else {
+        myself.devicesOther.push(d);
+      }
+    })
   };
 
   var failure = function (error) {
